3626296a217ce327631d4cebbf9bbd95ff277216
Siarhei Ivaneichyk <Siarhei_Ivaneichyk@epam.com>
2020-08-03 22:26:07 +0300

    Add new plugins, set up optimization only for production, add sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const sass = require('node-sass');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
@@ -16,23 +18,30 @@ module.exports = function (env, options) {
             filename: "./bundle.js",
             path: path.join(__dirname, "dist"),
         },
-        plugins: [new MiniCssExtractPlugin()],
-        optimization: {
-            minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
-          },
+        plugins: [new MiniCssExtractPlugin(), new HtmlWebpackPlugin({
+            title: 'Webpack Task2',
+            filename: '../dist/index.html'
+        })],
         module: {
             rules: [
                 {
-                    test: /.\js$/,
+                    test: /.\jsx$/,
                     exclude: /node_modules/,
                     use: [
                         { loader: "babel-loader" }
                     ]
                 },
                 {
-                    test: /\.css$/i,
-                    use: [MiniCssExtractPlugin.loader, 'css-loader'],
-                }
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                      // Creates `style` nodes from JS strings
+                      'style-loader',
+                      // Translates CSS into CommonJS
+                      MiniCssExtractPlugin.loader, 'css-loader',
+                      // Compiles Sass to CSS
+                      'sass-loader',
+                    ],
+                  },
             ]
         },
         resolve: {
@@ -41,16 +50,18 @@ module.exports = function (env, options) {
         devServer: {
             contentBase: path.join(__dirname, "dist"),
             open: true,
+            port: 8986,
         },
         watch: true,
-        optimization: {
+        optimization: isProd ? {
             minimize: true,
             minimizer: [
               new TerserPlugin({
                 test: /.\js$/,
               }),
+              new OptimizeCSSAssetsPlugin({}),
             ],
-          },
+          } : {},
     }
     return config;
 }
